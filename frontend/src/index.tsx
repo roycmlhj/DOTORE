6bfd2fd47f2afd790ce5c059e86d8dd7085f9f4a
한지희 <g2hhh2ee@gmail.com>
2022-03-20 17:15:51 +0900

    refactor: 불필요 코드 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,26 +5,14 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 
-// import { Provider } from 'react-redux'
-// import Blocklist from './components/Blocklist'
-// import { NetworkContextName } from './constants/misc'
 import { NetworkContextName } from "./constant/misc";
-// import store from './state'
-// import ApplicationUpdater from './state/application/updater'
-// import ListsUpdater from './state/lists/updater'
-// import LogsUpdater from './state/logs/updater'
-// import TransactionUpdater from './state/transactions/updater'
-// import UserUpdater from './state/user/updater'
-// import ThemeProvider, { ThemedGlobalStyle } from './theme'
-// import RadialGradientByChainUpdater from './theme/RadialGradientByChainUpdater'
 import getLibrary from "./utils/getLibrary";
-// export const NetworkContextName = 'NETWORK';
+
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
 
 if (!!window.ethereum) {
-  window.ethereum.autoRefreshOnNetworkChange = false
+  window.ethereum.autoRefreshOnNetworkChange = false;
 }
-
 
 // function getLibrary(provide: ExternalProvider) {
 //   const library = new Web3Provider(provide, "any");
@@ -33,7 +21,6 @@ if (!!window.ethereum) {
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
     <Web3ReactProvider getLibrary={getLibrary}>
       <Web3ProviderNetwork getLibrary={getLibrary}>
         <RecoilRoot>
@@ -41,11 +28,6 @@ ReactDOM.render(
         </RecoilRoot>
       </Web3ProviderNetwork>
     </Web3ReactProvider>
-    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
-//   serviceWorkerRegistration.register()
-// }
