c1308e0d163f16335c546fe75c1a8180f0bc4399
한지희 <g2hhh2ee@gmail.com>
2022-03-20 17:13:24 +0900

    feat: index.tsx에 getLibrary() 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,51 @@
 import { ExternalProvider, Web3Provider } from "@ethersproject/providers";
-import { Web3ReactProvider } from "@web3-react/core";
+import { Web3ReactProvider, createWeb3ReactRoot } from "@web3-react/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 
-function getLibrary(provide: ExternalProvider) {
-  const library = new Web3Provider(provide, "any");
-  return library;
+// import { Provider } from 'react-redux'
+// import Blocklist from './components/Blocklist'
+// import { NetworkContextName } from './constants/misc'
+import { NetworkContextName } from "./constant/misc";
+// import store from './state'
+// import ApplicationUpdater from './state/application/updater'
+// import ListsUpdater from './state/lists/updater'
+// import LogsUpdater from './state/logs/updater'
+// import TransactionUpdater from './state/transactions/updater'
+// import UserUpdater from './state/user/updater'
+// import ThemeProvider, { ThemedGlobalStyle } from './theme'
+// import RadialGradientByChainUpdater from './theme/RadialGradientByChainUpdater'
+import getLibrary from "./utils/getLibrary";
+// export const NetworkContextName = 'NETWORK';
+// const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
+
+if (!!window.ethereum) {
+  window.ethereum.autoRefreshOnNetworkChange = false
 }
+
+
+// function getLibrary(provide: ExternalProvider) {
+//   const library = new Web3Provider(provide, "any");
+//   return library;
+// }
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
     <Web3ReactProvider getLibrary={getLibrary}>
+      {/* <Web3ProviderNetwork getLibrary={getLibrary}> */}
       <RecoilRoot>
         <App />
       </RecoilRoot>
+      {/* </Web3ProviderNetwork> */}
     </Web3ReactProvider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
+//   serviceWorkerRegistration.register()
+// }
--- frontend/src/lib/connectors.tsx
+++ frontend/src/lib/connectors.tsx
@@ -1,33 +1,33 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 
-export enum ConnectorNames {
-    Metamask = 'Metamask',
-}
+// export enum ConnectorNames {
+//     Metamask = 'Metamask',
+// }
 
-const POLLING_INTERVAL = 12000;
-const RPC_URLS: { [chainId: number]: string } = {
-    31221: process.env.REACT_APP_RPC_URL_31 as string,
-}
+// const POLLING_INTERVAL = 12000;
+// const RPC_URLS: { [chainId: number]: string } = {
+//     31221: process.env.REACT_APP_RPC_URL_31 as string,
+// }
 
-export const injected = new InjectedConnector({ supportedChainIds: [31221, 31] });
+export const injected = new InjectedConnector({ supportedChainIds: [31221] });
 
-export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
-    [ConnectorNames.Metamask]: injected,
-}
+// export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
+//     [ConnectorNames.Metamask]: injected,
+// }
 
-const networks = {
-    ssafy: {
-        // 31221
-        // 0x79f5
-        chainId: `0x${Number(31221).toString(16)}`,
-        chainName: "SSAFY 테스트넷",
-        nativeCurrency: {
-            name: "SSF",
-            symbol: "SSF",
-            decimal: 31221,
-            // decimal: 18,
-        },
-        rpcUrls: ["http://20.196.209.2:8545"],
-        // blockExplorerUrls: [""]
-    }
-}
+// const networks = {
+//     ssafy: {
+//         // 31221
+//         // 0x79f5
+//         chainId: `0x${Number(31221).toString(16)}`,
+//         chainName: "SSAFY 테스트넷",
+//         nativeCurrency: {
+//             name: "SSF",
+//             symbol: "SSF",
+//             decimal: 31221,
+//             // decimal: 18,
+//         },
+//         rpcUrls: ["http://20.196.209.2:8545"],
+//         // blockExplorerUrls: [""]
+//     }
+// }
